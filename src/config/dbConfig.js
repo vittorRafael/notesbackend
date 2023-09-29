c9889bb9741d0dda62f069670cfc802067fb68f9
Vittor Rafael Pinho Sales <94492049+vittorRafael@users.noreply.github.com>
2023-09-29 09:28:55 -0300

    Update dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,7 +1,7 @@
 //
 const mongoose = require('mongoose');
 const dbConfig =
-  'mongodb+srv://rafael:usuario@cluster0.sxzcuy8.mongodb.net/annotations?retryWrites=true&w=majority';
+  'mongodb+srv://rafael:kgTKAkFCwVfaZPpB@cluster0.sxzcuy8.mongodb.net/annotations?retryWrites=true&w=majority';
 
 mongoose.set('strictQuery', true);
 const connection = mongoose.connect(dbConfig, {
